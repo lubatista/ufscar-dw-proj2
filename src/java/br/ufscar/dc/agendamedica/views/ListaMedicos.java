805507de5ac2b97b998784c6b7c26afcd2b4f141
Pedromauad <phdmauad@gmail.com>
2018-06-20 00:47:33 -0300

    Listagem de Médicos

---
+++ src/java/br/ufscar/dc/agendamedica/views/ListaMedicos.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufscar.dc.agendamedica.views;
+
+import br.ufscar.dc.agendamedica.beans.Medico;
+import br.ufscar.dc.agendamedica.dao.MedicoDAO;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author duduoliverio
+ */
+@Named
+@RequestScoped
+public class ListaMedicos implements Serializable {
+
+    List<Medico> listaMedicos;
+    @Inject
+    MedicoDAO medicoDAO;
+
+    private List<Medico> medicosFiltrados;
+    
+    private List<String> especialidade;
+
+    Medico medico;
+
+    
+    @PostConstruct
+    public void init() {
+        try {
+            especialidade = medicoDAO.listarTodasEspecialidade();
+        } catch (SQLException ex) {
+            Logger.getLogger(ListaMedicos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public List<Medico> getListaMedicos() {
+        return listaMedicos;
+    }
+
+    public void setListaMedicos(List<Medico> listaMedicos) {
+        this.listaMedicos = listaMedicos;
+    }
+
+    public String verTodosMedicos() throws SQLException, NamingException {
+        listaMedicos = medicoDAO.listarTodosMedicos();
+        return "listaMedicos";
+    }
+
+    public List<Medico> getMedicosFiltrados() {
+        return medicosFiltrados;
+    }
+
+    public void setMedicosFiltrados(List<Medico> medicosFiltrados) {
+        this.medicosFiltrados = medicosFiltrados;
+    }
+
+    public List<String> getEspecialidades() {
+        return especialidade;
+    }
+    
+    /* public String verTodosMedicosEspecialidade(String especialidade) throws SQLException, NamingException {
+
+        listaMedicos = medicoDAO.listarMedicosEspecialidade(especialidade);
+        return "listaMedicos";
+    }*/
+}
